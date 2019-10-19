63343754d576bee25246b3b54a6c8f7347ada11d
Hidde Wieringa <hidde@hiddewieringa.nl>
2019-10-19 15:22:37 +0200

    Enable modules

---
+++ src/main/kotlin/module-info.java
@@ -0,0 +1,19 @@
+module com.hiddewieringa.elevator {
+    requires org.axonframework.messaging;
+    requires org.axonframework.modelling;
+    requires org.axonframework.spring;
+    requires org.axonframework.eventsourcing;
+    requires kotlin.stdlib;
+    requires spring.web;
+    requires spring.beans;
+    requires spring.context;
+    requires java.persistence;
+    requires org.hibernate.orm.core;
+    requires com.fasterxml.jackson.annotation;
+    requires spring.data.commons;
+    requires spring.boot.autoconfigure;
+    requires org.slf4j;
+    requires spring.boot;
+    requires reactor.core;
+    requires spring.webflux;
+}
